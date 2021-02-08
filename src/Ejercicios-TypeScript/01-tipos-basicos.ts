a8632cdbf47cbf7303c397fb884b40472364165c
crisgarm <crisgarm9@gmail.com>
2021-02-08 17:43:18 +0100

    Add exercises 01-02 about TypeScript

---
+++ src/Ejercicios-TypeScript/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let myName: string = "Cristina";
+let charge: number | string = 90;
+let isAlive: boolean = true;
+
+charge = "FULL";
+
+console.log(myName, charge);
