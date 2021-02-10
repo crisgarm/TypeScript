6172d6a207f72295efa1aaa17251205bae17f3f0
crisgarm <crisgarm9@gmail.com>
2021-02-10 16:45:20 +0100

    Add exercise 06 about destructing

---
+++ src/Ejercicios-TypeScript/06-desestructuracion-funciones.ts
@@ -0,0 +1,34 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Product {
+  description: string;
+  price: number;
+}
+
+const phone: Product = {
+  description: "Samsung 8",
+  price: 170,
+};
+
+const tablet: Product = {
+  description: "iPad Air",
+  price: 350,
+};
+
+function getVAT(products: Product[]): [number, number] {
+  let total = 0;
+  products.forEach(({ price }) => {
+    total += price;
+  });
+
+  return [total, total * 0.15];
+}
+
+const items = [phone, tablet];
+
+const [total, vat] = getVAT(items);
+
+console.log("Total: ", total);
+console.log("VAT: ", vat);
